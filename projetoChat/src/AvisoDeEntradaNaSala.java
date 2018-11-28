488e88250d67977e3db95a1c6b86a0f410b3d420
DudaElias <42540760+DudaElias@users.noreply.github.com>
2018-11-28 12:55:08 -0200

    Parte da Duda do chatDePOO

---
+++ projetoChat/src/AvisoDeEntradaNaSala.java
@@ -0,0 +1,15 @@
+public class AvisoDeEntradaNaSala implements Coisa
+{
+   protected String nome;
+   public AvisoDeEntradaNaSala(String nome) throws Exception
+   {
+       if(nome == null)
+           throw new Exception("Nome não inserido corretamente!");
+       this.nome = nome;
+   }
+   
+   public String toString()
+   {
+       return "O usuário " + this.nome + " entrou na sala";
+   }
+}
